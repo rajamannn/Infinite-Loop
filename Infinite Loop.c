2a714a02f769ce8bacea12a181dc528a78fa2139
Aman Raj <160530800+rajamannn@users.noreply.github.com>
2024-10-29 19:46:10 +0530

    Add files via upload

---
+++ Infinite Loop.c
@@ -0,0 +1,19 @@
+//Program for infinite loop using for, while, do-while
+#include <stdio.h>
+
+int main()
+{
+    for (;;) {
+        printf("Hello Everyone");
+    }
+
+    while (1) {
+        printf("Hello Everyone");
+    }
+
+    do {
+        printf("Hello Everyone");
+    } while (1);
+
+    return 0;
+}
